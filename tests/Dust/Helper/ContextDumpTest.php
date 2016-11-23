55035065230a9e54e7b0b96f7e54d14c915bd855
Kerry James <operations@bloafer.com>
2016-11-23 20:21:47 +0000

    PHP5.3

--- tests/Dust/Helper/ContextDumpTest.php
+++ tests/Dust/Helper/ContextDumpTest.php
@@ -3,20 +3,20 @@ namespace Dust\Helper;
 
 class ContextDumpTest extends \Dust\DustTestBase {
     public function testContextDump() {
-        $ctx = (object)[
-            "outer" => (object)[
+        $ctx = (object)array(
+            "outer" => (object)array(
                 "meh" => 'meh',
-                "inner" => (object)[
+                "inner" => (object)array(
                     "key" => 'current',
                     "output" => 'output'
-                ]
-            ]
-        ];
+                )
+            )
+        );
         $templ = '{#outer outerFoo="bar"}{#inner:meh innerFoo="bar"}{@contextDump key=key to=output /}{/inner}{/outer}';
         //I really don't care what the output is, I just wanna run it
         $this->dust->renderTemplate($this->dust->compile($templ), $ctx);
         $ctx->outer->inner->key = 'full';
         $this->dust->renderTemplate($this->dust->compile($templ), $ctx);
     }
-    
+
 }
--- tests/Dust/Helper/IfHelperTest.php
+++ tests/Dust/Helper/IfHelperTest.php
@@ -6,9 +6,9 @@ class IfHelperTest extends \Dust\DustTestBase {
     public function testIfHelper() {
         $this->dust->helpers['if'] = new Helper\IfHelper();
         $templ = '{@if cond="{x} < {y} || {x} < 3"}yes{:else}no{/if}';
-        $this->assertTemplate('yes', $templ, (object)["x" => 12, "y" => 15]);
-        $this->assertTemplate('yes', $templ, (object)["x" => 2, "y" => 1]);
-        $this->assertTemplate('no', $templ, (object)["x" => 7, "y" => 5]);
+        $this->assertTemplate('yes', $templ, (object)array("x" => 12, "y" => 15));
+        $this->assertTemplate('yes', $templ, (object)array("x" => 2, "y" => 1));
+        $this->assertTemplate('no', $templ, (object)array("x" => 7, "y" => 5));
     }
-    
+
 }
