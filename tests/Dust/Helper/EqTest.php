19d375a830d20314fff5fa715dba08b76403769b
Kerry James <operations@bloafer.com>
2016-11-23 20:25:01 +0000

    PHP5.3

--- tests/Dust/Helper/EqTest.php
+++ tests/Dust/Helper/EqTest.php
@@ -3,13 +3,13 @@ namespace Dust\Helper;
 
 class EqTest extends ComparisonTestBase {
     public function __construct() { parent::__construct('eq'); }
-    
+
     public function testEq() {
         $this->assertValidResult(12, 12, true);
         $this->assertValidResult(12, '12', true);
         $this->assertValidResult(12, false, false);
-        $this->assertValidResult([1, 2], [1, 2], true);
+        $this->assertValidResult(array(1, 2), array(1, 2), true);
         $this->assertValidResult('FOO', 'foo', false);
     }
-    
+
 }
